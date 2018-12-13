6720836f39a24249b88806fd5bcd426e4c540878
kranonetka <fakemail4steam@ya.ru>
2018-12-13 13:59:03 +0700

    [+] PANIC func

--- cmain.c
+++ cmain.c
@@ -1,18 +1,11 @@
 char * const VIDEO_MEM = (char *)0xb8000;
+unsigned short pointer = 0;
 
-unsigned int strlen(const char *str_addr)
-{
-	const char *tmp = str_addr;
-	while (*tmp)
-	{
-		++tmp;
-	}
-	return tmp - str_addr;
-}
+char buffer[1024] = {0};
 
 void clear_screen(char color)
 {
-	int i = 0;
+	unsigned short i = 0;
 	while (i < 80*25)
 	{
 		VIDEO_MEM[i << 1] = ' ';
@@ -20,17 +13,35 @@ void clear_screen(char color)
 		++i;
 	}	
 }
+unsigned strlen(const char *str_addr)
+{
+	const char *tmp = str_addr;
+	while (*tmp++);
+	return tmp - str_addr - 1;
+}
+
+void scroll()
+{
+	unsigned short tmp_ptr = 0;
+	while (tmp_ptr < 80*24)
+	{
+		VIDEO_MEM[tmp_ptr << 1] = VIDEO_MEM[(tmp_ptr << 1) + 80*2];
+		++tmp_ptr;
+	}
+	pointer = 80 * 24;
+}
+
 
 void print(const char* string)
 {
-	static unsigned short pointer = 0;
-	const unsigned int string_len = strlen(string);
-	int i = 0;
+//	static unsigned short pointer = 0;
+	const unsigned string_len = strlen(string);
+	unsigned i = 0;
 	while (i < string_len)
 	{
 		if (string[i] == '\n')
 		{
-			unsigned short est_spaces =
+			unsigned char est_spaces =
 			80 - (pointer % 80)
 			?
 			(80 - (pointer % 80))
@@ -45,7 +56,7 @@ void print(const char* string)
 		}
 		else if (string[i] == '\t')
 		{
-			unsigned short est_spaces =
+			unsigned char est_spaces =
 			8 - (pointer & 7)
 			?
 			(8 - (pointer & 7))
@@ -65,6 +76,8 @@ void print(const char* string)
 		}
 		if (pointer == 80*25)
 		{
+			scroll();
+/*
 			unsigned short tmp_ptr = 0;
 			while (tmp_ptr < 80*24)
 			{
@@ -72,21 +85,78 @@ void print(const char* string)
 				++tmp_ptr;
 			}
 			pointer = 80 * 24;
+*/
 		}
 		++i;
 	}
 }
+
+void reverse(char* str, int length)
+{
+	unsigned start = 0;
+	unsigned end = length - 1;
+	while (start < end)
+	{
+		char tmp = str[start];
+		str[start++] = str[end];
+		str[end--] = tmp;
+	}
+}
+
+char* itoa (int value, char * dest, const unsigned base)
+{
+	unsigned char i = 0;
+	unsigned char isNegative = 0;
+
+	if (!value)
+	{
+		dest[i++] = '0';
+		dest[i] = '\0';
+		return dest;
+	}
+	
+	if (value < 0 && base == 10)
+	{
+		isNegative = 1;
+		value = -value;
+	}
+
+	while (value)
+	{
+		unsigned char digit = value % base;
+		dest[i++] = digit + '0';
+		value /= base;
+	}
+
+	if (isNegative)
+	{
+		dest[i++] = '-';
+	}
+	
+	dest[i] = '\0';
+	reverse(dest, i);
+	return dest;
+}
+
+void PANIC(const char *err_msg, const char *filename, int line)
+{
+	itoa(line, buffer, 10);
+	while (1)
+	{
+		print("PANIC: ");
+		print(filename);
+		print(":");
+		print(buffer);
+		print(": error: ");
+		print(err_msg);
+		print("\n");
+	}
+}
+
 int cmain()
 {
 	clear_screen(0x1e);
-	int i = 0;
-	print("\ttest");
-	while (i < 20)
-	{
-		print("\n");
-		++i;
-	}
-	print("\n\tmov eax,5\n\tinc eax\n\t");
-	print("bbb");	
+	PANIC("test panic", __FILE__, __LINE__);
 	return 0;
+
 }
