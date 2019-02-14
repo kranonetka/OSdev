5345a95c608609cfeafbf14041690fdff55a47c0
kranonetka <fakemail4steam@ya.ru>
2019-02-14 19:45:09 +0700

    [-] Trash from MBR

--- bootblock.s
+++ bootblock.s
@@ -1,7 +1,6 @@
 ; vim:ft=nasm
 [BITS 16]
 org 0x7c00
-
 	;Инициализация сегментных регистров
 	jmp 0x0:($+5)	;Инициализация регистра cs
 	xor ax, ax
@@ -10,60 +9,17 @@ org 0x7c00
 	mov fs, ax
 	mov gs, ax
 	mov ss, ax
-bootblock:
-;start_with_ints:
-;	mov ax, 0x0003
-;	int 0x10	;change video mode
-;
-;	mov ah, 0x0b
-;	mov bx, 0x0001
-;	int 0x10	;change background
-;
-;	mov ax, 0x1301
-;	mov bx, 0x001e
-;	mov cx, msg_len
-;	xor dx, dx
-;	mov bp, msg
-;	int 0x10	;print string
-;
-;start_without_ints:
-;	;video memory at 0xb8000
-;	mov ax, 0xb800
-;	mov ds, ax
-;
-;	mov cx, 25*80
-;	xor bx, bx
-;.clear_screen:
-;	mov word [bx], 0x1e20
-;	db 0x43, 0x43
-;	loop .clear_screen
-;
-;	mov si, msg
-;	mov bx, 0xb800
-;	mov gs, bx
-;	xor bx, bx
-;	mov ds, bx
-;	mov ah, 0x1e
-;.print_message:
-;	lodsb
-;	test al, al
-;	jz .read_sectors
-;	mov [gs:bx], ax
-;	db 0x43, 0x43
-;	jmp .print_message
-.read_sectors:
+
+read_sectors:
 	mov bx, 0x07e0
 	mov es, bx
 	xor bx, bx
-	mov ax, 0x0210	;read 16 sectors
+	mov ax, 0x02ff	;read 256 sectors
 	mov cx, 0x0002	;cylinder 0, sector 2
 	mov dx, 0x0080	;head 0, dl = 80 - disk on ch0
 	int 0x13	;read to 0x07e00
 
-jmp 0x7e00	;Переход в защищенный режим
-
-;msg: db "Hello, world!", 0
-;msg_len: equ $ - msg - 1
+jmp 0x7e00	;Переход на ядро
 
 times 510 - ($ - $$) db 0
 db 0x55, 0xaa
