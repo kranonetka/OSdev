5765f6b3d5430b4efba2d847425802b968c8ab85
kranonetka <fakemail4steam@ya.ru>
2019-05-13 16:22:50 +0700

    [+] alloc, free funcs

--- common.c
+++ common.c
@@ -1,4 +1,8 @@
 #include "common.h"
+#include "panic.h"
+
+static unsigned int alloc_mem = 0x47e00; //256 kB from 0x7e00
+static const unsigned int alloc_limit = 0x7ffff;
 
 void outb(const unsigned short int port, const unsigned char value)
 {
@@ -29,4 +33,20 @@ unsigned short int inw(const unsigned short int port)
 		: "dN" (port)
 	);
 	return retval;
-} 
+}
+
+unsigned int malloc(unsigned int size)
+{
+	if (alloc_mem + size > alloc_limit)
+	{
+		PANIC("Out of memory");
+	}
+	unsigned int ret_mem = alloc_mem;
+	alloc_mem += size;
+	return ret_mem;	
+}
+
+void free(unsigned int ptr)
+{
+	return;
+}
--- common.h
+++ common.h
@@ -5,3 +5,6 @@ typedef enum { true=1, false=0} bool;
 void outb(const unsigned short int port, const unsigned char value);
 unsigned char inb(const unsigned short int port);
 unsigned short int inw(const unsigned short int port);
+
+unsigned int malloc(unsigned int size);
+void free(unsigned int ptr);
